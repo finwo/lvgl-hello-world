1328b80830074fb6180ae6ad4b67ac22661b32bc
Yersa Nordman <finwo@pm.me>
2025-08-16 16:37:06 +0200

    Load sono font to see if windows stb assert errors are font-specific or global

--- src/main.c
+++ src/main.c
@@ -81,22 +81,23 @@ int app_setup() {
   // Set background to recognizable color
   lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x2255AA), LV_PART_MAIN);
 
-  // // Check if we can load the font
-  // char *font_file;
+  // Check if we can load the font
+  char *font_file;
   // asprintf(&font_file, "%s/public-pixel.ttf", appDir);
-  // log_debug("Target font file: %s\n", font_file);
-  // if (!file_exists(font_file, "r")) {
-  //   log_fatal("Font file could not be read");
-  //   exit(1);
-  // }
+  asprintf(&font_file, "%s/sono.ttf", appDir);
+  log_debug("Target font file: %s\n", font_file);
+  if (!file_exists(font_file, "r")) {
+    log_fatal("Font file could not be read");
+    exit(1);
+  }
 
-  // // Actually load the font
-  // int font_size = 24;
-  // struct buf *font_data = file_get_contents(font_file);
-  // lv_font_t *customFont = lv_tiny_ttf_create_data(font_data->data, font_data->len, font_size);
+  // Actually load the font
+  int font_size = 24;
+  struct buf *font_data = file_get_contents(font_file);
+  lv_font_t *customFont = lv_tiny_ttf_create_data(font_data->data, font_data->len, font_size);
   lv_style_t *fontStyle = calloc(1, sizeof(lv_style_t));
   lv_style_init(fontStyle);
-  // lv_style_set_text_font(fontStyle, customFont);
+  lv_style_set_text_font(fontStyle, customFont);
   lv_style_set_text_color(fontStyle, lv_color_hex(0xFFFFFF));
   lv_obj_add_style(lv_screen_active(), fontStyle, 0);
 
